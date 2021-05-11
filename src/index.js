3ebd0c27615f556b389bfd04eb713d492c61cada
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2021-05-11 22:37:38 +0900

    テンプレート文字列などの使用方法

--- src/index.js
+++ src/index.js
@@ -1,10 +1,177 @@
-import "./styles.css";
+// const,let等の変数宣言
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "varの変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "var変数を上書き";
+// console.log(val2);
+
+// letは変数宣言不可能
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// constで定義したオブジェクトはプロパティの変更が可能
+//  const val4 = {
+//     name:"じゃけえ",
+//     age: 28,
+// };
+// val4.name = "じゃ"
+
+// console.log(val4);
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ['dog','cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+// テンプレート文字列
+
+// const name = "ジャケ";
+// const age = 28;
+
+// // 「私の名前はジャケです。年齢は28歳です。」
+
+// // テンプレート文字列を使う
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+
+// アロー関数
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("です"));
+
+// アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 =(num1,num2) => {
+//   return num1 + num2 ;
+
+// };
+
+// console.log(func3(10,20));
+
+// 分割代入
+
+// const myProfile ={
+//   name: "ジャケ",
+//   age: 28,
+// };
+// const message1 = `名前は${myProfile.name}です。`
+// console.log(message1);
+
+// const { name } = myProfile;
+// const message2 = `名前は${name}です。`
+// console.log(message2);
+
+// デフォルト値
+// const sayHello = (name = "guest") => console.log(`こんにちは！${name}さん!`)
+// sayHello();
+
+// スプレッド構文...
+
+// 配列の展開
+
+// const arr1 = [1,2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0],arr1[1]);
+// sumFunc(...arr1);
+
+// 配列に...ドット３つついてたら順番に処理しているのだなと思っておく
+
+// まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3]  =arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10 , 20];
+// const arr5 = [30 , 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 =[...arr4,...arr5];
+// console.log(arr7);
+
+// =でコピーすると元のデータも変更した際に影響を及ぼす
+
+// mapやfilterを使った配列の処理
+// const nameArr = ["田中","山田","じゃけえ"];
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name, index)=> console.log (`${index + 1}番目は${name}です`));
+
+// const numArr =[1 ,2, 3, 4, 5];
+// const newNumArr = numArr.filter((num)=>{
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name)=>{
+//   if (name === "じゃけえ"){
+//     return name
+//   } else{
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr)
+
+// 三項演算子
+
+// ある条件　？　条件がtrueの時　：　条件がfalseの時
+// const val1 = 1 > 0 ? `trueです`　: `falseです`;
+// console.log(val1);
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === `number` ? num.toLocaleString() : `数値を入力してください`;
+// console.log(formattedNum)
+
+// typeofは変数の型がどういう種類なのかを自動で判定してくれる
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '１００を超えています！！' :　'許容範囲内です';
+
+// }
+// console.log(checkSum(50,100));
+
+// 論理演算子の本当の意味を知ろう　$$ ||
+
+// const flag1 = true;
+// const flag2 = false;
+//  if (flag1 || flag2){
+//    console.log("１か２はtrue")
+//  }
+
+// ||は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+//nullはfalse判定になる
+
+// &&は左側がtrueなら右側を返す
+// const num2 =　"111";
+// const fee2 = num2  &&  "何か設定されました";
+// console.log(fee2);
